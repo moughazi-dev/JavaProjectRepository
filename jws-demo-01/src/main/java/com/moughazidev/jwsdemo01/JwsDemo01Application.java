4fd242fa80b18ebc6e4d78f398c881d41f782677
moughazi-dev <abdou.maghazi@gmail>
2019-09-21 22:03:16 +0100

    My First Netbeans Commit with git

---
+++ jws-demo-01/src/main/java/com/moughazidev/jwsdemo01/JwsDemo01Application.java
@@ -0,0 +1,13 @@
+package com.moughazidev.jwsdemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwsDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwsDemo01Application.class, args);
+	}
+
+}
